92740e2a41c3cd828acb16b4adcefc5e7ed150ae
logapartha2 <logapartha2@yahoo.com>
2021-10-04 15:29:17 +0000

    VPC creation 10

---
+++ AWS_VPC.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-1"
+  #access_key = "AKIA2UWODIVGG2K22MEF"
+  #secret_key = "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"
+}
+
+# Create a VPC
+resource "aws_vpc" "MAIN" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+    Name = "partha_vpc"
+  }
+}
+
